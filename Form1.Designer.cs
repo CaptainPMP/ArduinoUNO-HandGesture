4dc8204bd5e03022d98e2873efa80f1b04cd456c
CaptainPMP <scikung8@gmail.com>
2023-01-21 16:55:28 +0700

    First commit + arduino code still give wrong value (it does not real sync)

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace HandGestureMouseControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.portBox = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StartBTN = new System.Windows.Forms.Button();
+            this.ACCX = new System.Windows.Forms.Label();
+            this.ACCY = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM PORT :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sensitivity :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ACC.X :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(321, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ACC.Y :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(127, 304);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(256, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "For Exit Hand Gesture Mode Press";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(423, 304);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ctrl + Z";
+            // 
+            // portBox
+            // 
+            this.portBox.FormattingEnabled = true;
+            this.portBox.Location = new System.Drawing.Point(242, 104);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(121, 28);
+            this.portBox.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StartBTN
+            // 
+            this.StartBTN.Location = new System.Drawing.Point(131, 373);
+            this.StartBTN.Name = "StartBTN";
+            this.StartBTN.Size = new System.Drawing.Size(158, 44);
+            this.StartBTN.TabIndex = 7;
+            this.StartBTN.Text = "Start";
+            this.StartBTN.UseVisualStyleBackColor = true;
+            this.StartBTN.Click += new System.EventHandler(this.StartBTN_Click);
+            // 
+            // ACCX
+            // 
+            this.ACCX.AutoSize = true;
+            this.ACCX.Location = new System.Drawing.Point(203, 237);
+            this.ACCX.Name = "ACCX";
+            this.ACCX.Size = new System.Drawing.Size(18, 20);
+            this.ACCX.TabIndex = 8;
+            this.ACCX.Text = "0";
+            // 
+            // ACCY
+            // 
+            this.ACCY.AutoSize = true;
+            this.ACCY.Location = new System.Drawing.Point(393, 237);
+            this.ACCY.Name = "ACCY";
+            this.ACCY.Size = new System.Drawing.Size(18, 20);
+            this.ACCY.TabIndex = 9;
+            this.ACCY.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ACCY);
+            this.Controls.Add(this.ACCX);
+            this.Controls.Add(this.StartBTN);
+            this.Controls.Add(this.portBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox portBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button StartBTN;
+        private System.Windows.Forms.Label ACCX;
+        private System.Windows.Forms.Label ACCY;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace HandGestureMouseControl
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort myport;
+        private string in_data;
+        private Boolean IsStart = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            string[] unique_ports = ports.Distinct().ToArray();
+            myport = new SerialPort();
+            myport.BaudRate = 9600;
+            myport.PortName = unique_ports[0];
+            myport.Parity = Parity.None;
+            myport.DataBits = 8;
+            myport.StopBits = StopBits.One;
+            myport.DataReceived += Myport_DataReceived;
+            timer1.Start();
+
+            try
+            {
+                myport.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+            foreach (string port_name in unique_ports)
+            {
+                portBox.Items.Add(port_name);
+            }
+
+        }
+
+        private void Myport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            in_data = myport.ReadLine();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (IsStart)
+            {
+                string[] ACC = in_data.Split(',');
+                ACCX.Text = ACC[0];
+                ACCY.Text = ACC[1];
+                Cursor.Position = new System.Drawing.Point(Cursor.Position.X + Convert.ToInt32(ACC[0]), Cursor.Position.Y + Convert.ToInt32(ACC[1]));
+            }
+        }
+
+        private void StartBTN_Click(object sender, EventArgs e)
+        {
+            IsStart = true;
+            //myport.PortName = portBox.GetItemText(portBox.SelectedItem); => I want to use combobox to choose comport
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if(keyData == (Keys.Control | Keys.Z) || keyData == Keys.Escape)
+            {
+                Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
